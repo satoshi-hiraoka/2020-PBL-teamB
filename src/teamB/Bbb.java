7fd37ae554e71b88219da5704872be61596438f3
金井　彰太 <@tsd-KANAI>
2020-07-09 13:39:49 +0900

    add 	S0024.html,S0025.html fix 	S0010.html,S0011.html,S0020.html 	S0021.htmlS0022.html,S0023.html 		セレクトボックスの中身を画面設計書準拠するように修正

--- src/teamB/Bbb.java
+++ src/teamB/Bbb.java
@@ -1,6 +1,7 @@
 package teamB;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,19 +14,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Bbb")
 public class Bbb extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Bbb() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Bbb() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -33,7 +35,8 @@ public class Bbb extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
