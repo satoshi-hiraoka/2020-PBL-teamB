ad04a68494c161e449d84b1cd18149d8696b193b
金井　彰太 <@192.168.0.17>
2020-07-15 14:10:32 +0900

    fix 	User.java 		項目追加

---
+++ src/com/abc/asms/User.java
@@ -0,0 +1,122 @@
+package com.abc.asms;
+
+public class User {
+	private String id;
+	private String name;
+	private String account_id;
+	private String mail;
+	private String authority;
+	private String category_name;
+	private String category_id;
+	private String activ_flag;
+	private String sale_date;
+	private String trade_name;
+	private String unit_price;
+	private String sale_number;
+	private String note;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAccount_id() {
+		return account_id;
+	}
+
+	public void setAccount_id(String account_id) {
+		this.account_id = account_id;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	public String getCategory_name() {
+		return category_name;
+	}
+
+	public void setCategory_name(String category_name) {
+		this.category_name = category_name;
+	}
+
+	public String getCategory_id() {
+		return category_id;
+	}
+
+	public void setCategory_id(String category_id) {
+		this.category_id = category_id;
+	}
+
+	public String getActiv_flag() {
+		return activ_flag;
+	}
+
+	public void setActiv_flag(String activ_flag) {
+		this.activ_flag = activ_flag;
+	}
+
+	public String getSale_date() {
+		return sale_date;
+	}
+
+	public void setSale_date(String sale_date) {
+		this.sale_date = sale_date;
+	}
+
+	public String getTrade_name() {
+		return trade_name;
+	}
+
+	public void setTrade_name(String trade_name) {
+		this.trade_name = trade_name;
+	}
+
+	public String getUnit_price() {
+		return unit_price;
+	}
+
+	public void setUnit_price(String unit_price) {
+		this.unit_price = unit_price;
+	}
+
+	public String getSale_number() {
+		return sale_number;
+	}
+
+	public void setSale_number(String sale_number) {
+		this.sale_number = sale_number;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+}
