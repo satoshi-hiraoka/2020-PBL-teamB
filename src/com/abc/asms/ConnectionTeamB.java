0ec173df7a82e5ca724052958269f94f1cc0e5f6
sqkan <sqkan@192.168.0.29>
2020-07-16 11:16:17 +0900

    rename LoginConnection.java to ConnectionTeamB.java by iwane

--- src/com/abc/asms/ConnectionTeamB.java
+++ src/com/abc/asms/ConnectionTeamB.java
@@ -8,10 +8,10 @@ import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.sql.DataSource;
 
-public class LoginConnection {
+public class ConnectionTeamB implements interfaceConnectionTeamB {
 	private Connection con;
 
-	LoginConnection() throws ServletException, IOException {
+	ConnectionTeamB() throws ServletException, IOException {
 		try {
 			Context initContext = new InitialContext();
 			Context envContext = (Context) initContext.lookup("java:/comp/env");
@@ -24,7 +24,23 @@ public class LoginConnection {
 		}
 	}
 
-	public Connection getConnection() {
+	public Connection getCon() {
 		return con;
 	}
 }
+interface interfaceConnectionTeamB{
+	public Connection getCon();
+}
+
+//サーブレットに以下のソースコードを記述
+//	public クラス名 extends HttpServlet implements interfaceConnectionTeamB {
+//		private ConnectionTeamB cb;
+//	}
+//	public クラス名() throws ServletException, IOException {
+//		super();
+//		this.cb = new ConnectionTeamB();
+//	}
+//	public Connection getCon() {
+//		return this.cb.getCon();
+//	}
+//以降ConnectionはgetCon()で記述
