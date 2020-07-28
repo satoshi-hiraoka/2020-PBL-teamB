996be436f5c738a2a0e202a6e09065759b36eb8f
sqkan <sqkan@192.168.0.29>
2020-07-28 13:35:25 +0900

    delete InterfaceCheckLength.java,InterfaceConnectionTeamB.java by iwane

--- src/com/abc/asms/InterfaceCheckLength.java
+++
@@ -1,5 +0,0 @@
-package com.abc.asms;
-
-public interface InterfaceCheckLength {
-	public boolean checkLength(String value, int max);
-}
--- src/com/abc/asms/InterfaceConnectionTeamB.java
+++
@@ -1,7 +0,0 @@
-package com.abc.asms;
-
-import java.sql.Connection;
-
-public interface InterfaceConnectionTeamB {
-	 public Connection getCon();
-}
